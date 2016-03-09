021f3fa34659c0edb3f33b49b0b3bca5a14e41cf
Michael A DiGrigoli <mad2143@g.rit.edu>
2016-03-09 10:15:25 -0500

    added board and class

---
+++ FlameWars/FlameWars/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlameWars
+{
+    public class Board
+    {
+        public enum SpaceType { Resource, Card, Bonus, Stock, Random, Empty };
+    }
+}
---
+++ FlameWars/FlameWars/Path.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FlameWars
+{
+    public class Path
+    {
+        // Instance variables
+        private Vector2 pos;
+        private Rectangle boundaries;
+        private Color tint;
+        private SpaceType space;
+
+        // Properties
+        public SpaceType Space { get { this.space} }
+        public Vector2 Position { get { } set { } }
+        public Rectangle Bounds { get { } set { } }
+        public int X { get { } set { } }
+        public int Y { get { } set { } }
+        public Color Tint { get { } set { } }
+
+        // Constructor
+        Path() { }
+
+        // Method
+        public void Trigger()
+        {
+            Switch(space) {
+            Resource:
+                break;
+            Empty:
+                break;
+            }
+        }
+    }
+}
