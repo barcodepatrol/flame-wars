4c03f49c8c374c3dfb1a23bf44e83acc1057f4dd
DanteNardo <dcn5783@g.rit.edu>
2016-04-06 21:36:46 -0400

    Overview: Oops

--- FlameWars/FlameWars/States/Pause.cs
+++ FlameWars/FlameWars/States/Pause.cs
@@ -161,6 +161,9 @@ namespace FlameWars
 					{
 						case RESUME_INDEX:
 							StateManager.gameState = StateManager.GameState.Game;
+
+							// If a message existed before the pause
+							if (MessageExists) Message.Activate();
 							break;
 						case HOW_TO_INDEX:
 							StateManager.lastState = StateManager.gameState;
@@ -173,9 +176,6 @@ namespace FlameWars
 							StateManager.gameState = StateManager.GameState.Exit;
 							break;
 					}
-
-					// If a message existed before the pause
-					if (MessageExists) Message.Activate();
 				}
 				// Otherwise, reset the color
 				else
