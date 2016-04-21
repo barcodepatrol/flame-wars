6c789a93af04ba9e9ff9978f9b9f045fe1bb916f
DanteNardo <dcn5783@g.rit.edu>
2016-04-20 21:35:27 -0400

    Overview: Added Bond class.

--- FlameWars/FlameWars/Core/Player.cs
+++ FlameWars/FlameWars/Core/Player.cs
@@ -330,8 +330,6 @@ namespace FlameWars
 			finalPosition = BoardPosition + Dice.Roll(1);
 		}
 
-		
-
 		// This method merely updates the player's position (animation)
 		public void UpdateAnimation()
 		{
